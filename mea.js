68a4eb577493d93bfe29ce79c4407e0ba9cbfcde
mehug-wellness <admin@mehug.me>
2025-01-27 20:36:54 +0700

    Update mea.js

--- mea.js
+++ mea.js
@@ -6,12 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 35000;
 
 // SSO Configuration
-const CLIENT_ID = 'TUVBIENvbm5leHQ'; // Replace with your actual Client ID
-const CLIENT_SECRET = 'jWrCUnXbAvWB9K5yVK2Y4HTSyVVnWs5hMoSq'; // Replace with your actual Client Secret
+const CLIENT_ID = 'YjQXsBGqmNcPl0RqnNNVf2L0wblQa79u'; // already replace with actual Client ID
+const CLIENT_SECRET = '5BLpDIAkttwNCy4KubzJp1h8QYMdtvOnFM1FG08ACK8qa1KdvSVWJqiRO9fQPIH7'; // already replace with actual Client Secret
 const REDIRECT_URI = 'http://localhost:3000/callback'; // Replace with your registered Redirect URI
-const AUTH_URL = 'https://logindev.mea.or.th/oauth2/authorize'; // Staging
-const TOKEN_URL = 'https://logindev.mea.or.th/oauth2/token'; // Staging
-const PROFILE_URL = 'https://logindev.mea.or.th/profile'; // Staging
+const AUTH_URL = 'https://login.mea.or.th/oauth2/authorize'; // Production
+const TOKEN_URL = 'https://login.mea.or.th/oauth2/token'; // Production
+const PROFILE_URL = 'https://login.mea.or.th/profile'; // Production
 const JOTFORM_URL = 'https://form.jotform.com/243514830789465'; // Jotform URL
 
 // Step 1: Redirect User to SSO Login
